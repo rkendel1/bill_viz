4445301d5516d7910f109af26cd18fdbe435b4a0
Tyler Wiseman <wisemantyr@gmail.com>
2020-08-12 12:05:48 -0400

    reorganizing data for chart

--- static/js/buildChart.js
+++ static/js/buildChart.js
@@ -1,74 +1,36 @@
 ////script not referenced in index yet
 
-d3.json("/votes").then(function (data) { //replace csv with link
-    var demVotes = []
-    var repVotes = []
-    var indVotes = []
-    data.forEach(function(d){
-    demYes = d.democratic.yes
-    demNo = ~d.democratic.no
-    repYes = d.republican.yes
-    repNo = ~d.republican.no
-    indYes = d.independent.yes
-    indNo = ~d.independent.no
+votesData = []
 
-    demVotes.push(demYes, demNo)
-    repVotes.push(repYes, repNo)
-    indVotes.push(indYes, indNo)
+
+d3.json("/votes").then(function (data) {
+    data.forEach(function (d) {
+        //convert data to numeric
+        demYes = +d.democratic.yes
+        demNo = ~d.democratic.no //makes negative
+        repYes = +d.republican.yes
+        repNo = ~d.republican.no
+        indYes = +d.independent.yes
+        indNo = ~d.independent.no
+        votesData.push(
+            {"name": d.bill.bill_id,
+            "question": d.question,
+            "description": d.description,
+            "votes": [
+                {"category": "Democratic Yes",
+                "value": demYes },
+                {"category": "Democratic No",
+                "value": demNo},
+                {"category": "Republican Yes",
+                "value": repYes},
+                {"category": "Repulican No",
+                "value": repNo},
+                {"category": "Independent Yes",
+                "value": indYes},
+                {"category": "Independent No",
+                "value": indNo}
+            ]
+        })
     });
-
-    var voteCounts = [demVotes, repVotes, indVotes]
-
-    y.domain(data.map(function (d) { return d.bill.bill_id; })); //bill ids
-    x.domain(d3.extent(data, function (d) { return voteCounts; })); //range of all votes counts
-
-    var yAxis = svg.append("g")
-        .attr("class", "y-axis")
-        .attr("transform", "translate(" + x(0) + ",0)")
-        .append("line")
-        .attr("y1", 0)
-        .attr("y2", height);
-
-    var xAxis = svg.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", "translate(0," + (height + cfg.xAxisMargin) + ")")
-        .call(d3.axisBottom(x).tickSizeOuter(0));
-
-    var bars = svg.append("g")
-        .attr("class", "bars")
-
-    //////////START HERE//////////
-    bars.selectAll("rect")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "annual-growth")
-        .attr("x", function (d) {
-            return x(Math.min(0, voteCounts));
-        })
-        .attr("y", function (d) { return y(d.bill.bill_id); })
-        .attr("height", y.bandwidth())
-        .attr("width", function (d) {
-            return Math.abs(x(voteCounts) - x(0))
-        })
-        .style("fill", function (d) {
-            return colour(voteCounts)
-        });
-
-    var labels = svg.append("g")
-        .attr("class", "labels");
-
-    labels.selectAll("text")
-        .data(data)
-        .enter().append("text")
-        .attr("class", "bar-label")
-        .attr("x", x(0))
-        .attr("y", function (d) { return y(d.bill.bill_id) })
-        .attr("dx", function (d) {
-            return d.annual_growth < 0 ? cfg.labelMargin : -cfg.labelMargin;
-        })
-        .attr("dy", y.bandwidth())
-        .attr("text-anchor", function (d) {
-            return voteCounts< 0 ? "start" : "end";
-        })
-        .text(function (d) { return d.bill.bill_id })
+    console.log(votesData)
 });
